efed035a78df977545a3125d11fcc3b524c1a335
Davidcastel26 <davcastellanoslarios@gmail.com>
2023-12-17 13:53:50 -0600

    working customAdd

--- swift-crash-course-youtube/Clousers.playground/Contents.swift
+++ swift-crash-course-youtube/Clousers.playground/Contents.swift
@@ -9,9 +9,16 @@ add(20, 30)
 
 func customAdd(
     _ lhs: Int,
-    _ rhs: Int
+    _ rhs: Int,
     using functionSum: (Int, Int) -> Int
 ) -> Int {
     functionSum(lhs, rhs)
 }
 
+customAdd(
+    20,
+    30,
+    using: { (lhs: Int, rhs: Int) -> Int in
+        lhs + rhs
+    }
+)
